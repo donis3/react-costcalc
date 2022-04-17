e384ada2393168da6e124273e615c6f5996ce47c
donis3 <56738578+donis3@users.noreply.github.com>
2022-04-17 12:16:16 +0300

    added new department

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,13 +4,17 @@ import ModuleHeader from '../components/layout/ModuleHeader';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
 import useCompanyInfo from '../context/company/useCompanyInfo';
+import useIntl from '../hooks/common/useIntl';
 
 export default function HomePage() {
+	const { displayNumber } = useIntl();
 	let arr = [];
 	for (let i = 0; i < 10; i++) {
 		arr[i] = 'Dynamic Content Height Generation - Row: ' + (i + 1);
 	}
 	const { info } = useCompanyInfo();
+
+	const size = new Blob(Object.values(localStorage)).size;
 
 	return (
 		<Card className='w-full px-3 py-5 ' shadow='shadow-lg'>
@@ -18,6 +22,13 @@ export default function HomePage() {
 			<ModuleHeader text={info.name} module='home' role='main' />
 
 			<p className='p-2 leading-relaxed'>Welcome to cost calculator.</p>
+			<p>current local storage size: {displayNumber(size / 1024, 2)} kb</p>
+			<ul>
+				<li>TODO</li>
+				<li>Endproduct Form : slow</li>
+				<li>Packages Form : slow</li>
+				<li>Recipes Form : slow</li>
+			</ul>
 
 			<DayPicker />
 		</Card>
